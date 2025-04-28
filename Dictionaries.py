d3162d3f72d813c2cb917da6e92ffae89abd490b
Atlas Madhu <madhuamoli16@gmail.com>
2025-04-28 16:34:37 +0400

    Dictionaries and loops

--- Dictionaries.py
+++ Dictionaries.py
@@ -29,3 +29,36 @@ for x in lis:
 
 
 print(se)
+
+
+
+inventory={
+      "item1":100,
+      "item2":200,
+      "item3":300
+}
+
+inventory['item1']-=3
+print(inventory['item1'])
+
+if 'item4' not in inventory:
+      inventory['item4']=345
+      print(inventory)
+
+text = "I am a multimillionare a boy"
+word={}
+# for w in text.split():
+#       if w in word:
+#             word[w]+=1
+#       else:
+#             word[w]=1
+for w in text.split():
+    word[w]=  word.get(w,0)+1
+print(word)
+
+new_d = dict.fromkeys(word.keys(),'')
+print(new_d)
+liss = [33,44,55,32]
+dit = dict.fromkeys(liss,'yes')
+print(dit)
+
